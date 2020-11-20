51429d41842be6075c678b8ac37ee44a9c568bed
Karl Lew <karl@firepick.org>
2020-11-20 07:46:50 -0800

    snowbird

---
+++ scripts/build.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const { logger } = require('log-instance');
+const {
+    ExportHtml,
+} = require('scv-bilara');
+const APP_DIR = path.join(__dirname, '..');
+const API_DIR = path.join(APP_DIR, 'api');
+const TRANS_DIR = path.join(APP_DIR, 'translation/en/sujato/sutta');
+
+(async function(){
+    let eh = await new ExportHtml().initialize();
+    let suid = "thig4.1";
+    let lines = await eh.export(suid, 2);
+    console.log(lines);
+    let outPath = path.join(TRANS_DIR, 'kn/thig/thig4.1.html');
+    await fs.promises.writeFile(outPath, lines.join('\n'));
+})();
